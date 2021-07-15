ec5f490ea360b3cae372e90f81a89d9e7e822aeb
Vladimir <shaitan.vladimir@gmail.com>
2021-07-15 18:54:24 +0300

    init

---
+++ dom/dom_events/js/dom_events.js
@@ -0,0 +1,20 @@
+// function countRabbits() {
+//     for(let i=1; i<=3; i++) {
+//         alert("Кролик номер " + i);
+//     }
+// }
+
+// const elem = document.getElementById('elem');
+// elem.onclick = function() {
+//     alert('Спасибо');
+// };
+
+// elem.onclick = function(e) {
+//     console.log(e);
+// };
+
+// elem.addEventListener('click', {
+//     handleEvent(event) {
+//         alert(event.type + " на " + event.currentTarget);
+//     }
+// });
